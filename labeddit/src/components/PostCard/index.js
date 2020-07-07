280560da467e4a4a70ac07976e80a1731a814db2
Eros Pinder Valente <eros.pv@gmail.com>
2020-07-07 19:48:49 -0300

    comentarios sendo renderizados

--- labeddit/src/components/PostCard/index.js
+++ labeddit/src/components/PostCard/index.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Wrapper } from './styles'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import Container from "@material-ui/core/Container";
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { useHistory } from "react-router-dom";
+import { languages } from "../../languages";
+import LanguageContext from "../../contexts/LanguageContext";
+import { findByLabelText } from "@testing-library/react";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
-      width: "50%",  
+      marginTop: theme.spacing(8),
+      width: "100%",  
       minWidth: 275,
     },
     title: {
@@ -19,16 +24,17 @@ const useStyles = makeStyles({
     pos: {
       marginBottom: 12,
     },
-  });
+  }));
   
 
 const PostCard = (props) => {
+  const selectedLanguage = useContext(LanguageContext);
   const classes = useStyles();
   const history = useHistory();
 
 
   return (
-    <Wrapper>
+    <Container component="main" maxWidth="sm" >
         <Card className={classes.root} variant="outlined">
           <CardContent>
               <Typography className={classes.title} color="textSecondary" gutterBottom>
@@ -42,11 +48,13 @@ const PostCard = (props) => {
               </Typography>
               <hr/>
               <CardActions>
-                  <Typography>{props.commentsCount} comentários</Typography>
+                  <Typography>
+                      {props.commentsCount} {languages[selectedLanguage].commentCounterText}
+                  </Typography>
               </CardActions>
           </CardContent>
         </Card>
-    </Wrapper>
+    </Container>
   );
 };
 
--- labeddit/src/languages.js
+++ labeddit/src/languages.js
@@ -4,11 +4,13 @@ export const languages = {
     emailLabel: "Email Address",
     passwordLabel: "Password",
     signupText: "Don't have an account? Sign Up!",
+    commentCounterText: "comments"
   },
   pt: {
     loginLabel: "Entrar",
     emailLabel: "Endereço de Email",
     passwordLabel: "Palavra-chave",
     signupText: "Não tem uma conta? Cadastre-se!",
+    commentCounterText: "comentários"
   },
 };
