ea2a2d97bd29115d20f20540bca475ce58c97c1d
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-09 16:53:41 -0300

    spinner tava quebrado

--- labeddit/src/App.js
+++ labeddit/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useState } from "react";
 import Router from "./routes";
 import LanguageContext from "./contexts/LanguageContext";
 
--- labeddit/src/components/FeedPage/index.js
+++ labeddit/src/components/FeedPage/index.js
@@ -4,83 +4,79 @@ import useForm from "../../hooks/useForm";
 import usePosts from "../../hooks/usePosts";
 import { Helmet } from "react-helmet-async";
 import PostCard from "../PostCard";
-import styled from 'styled-components';
+import styled from "styled-components";
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import { createPost } from '../../functions/axios'
+import { createPost } from "../../functions/axios";
 import { FormControlLabel } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 const FormContainer = styled.form`
-display: grid;
-justify-items: center;
-margin-top: 20px;
+  display: grid;
+  justify-items: center;
+  margin-top: 20px;
+`;
 
-`
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+}));
 
 const FeedPage = () => {
- 
-  const [posts, getPosts] = usePosts();
+  const [posts, getPosts, resetForm] = usePosts();
   const history = useHistory();
-
-
+  const classes = useStyles();
   const [form, handleFormChange] = useForm({ text: "", title: "" });
-  console.log(form)
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        const response = await createPost(form);
-        console.log(response)
-        if (response) {
-          getPosts();
-          alert("Post criado com sucesso")
-          
-        } else {
-          alert("Erro ao criar Post")
-        }
-    };
-
-
-  const handlePostClick = (postId) => history.push(`posts/${postId}`);
-
-
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const success = await createPost(form);
+    if (success) {
+      resetForm();
+      getPosts();
+      alert("Post criado com sucesso");
+    } else {
+      alert("Erro ao criar Post");
+    }
+  };
+  console.log("oi");
   return posts.length > 0 ? (
     <div>
-
-      
-          <FormContainer onSubmit={handleFormSubmit}>
-            <p>
-              <TextField id="outlined-basic" variant="outlined"
-              placeholder="Escrever post"
-              type="text"
-              name="text"
-              onChange={handleFormChange}
-              />
-            </p>
-            
-            <p>
-              <TextField id="outlined-basic" variant="outlined"
-              placeholder="Título"
-              type="text"
-              name="title"
-              onChange={handleFormChange}
-              />
-            </p>
-            
-            <p>
-              <Button type="submit" variant="contained">Postar</Button>
-            </p>
-          </FormContainer>
-          
-     
-
-      
-
       <Helmet>
         <title>LabEddit</title>
       </Helmet>
+
+      <FormContainer onSubmit={handleFormSubmit}>
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          placeholder="Escrever post"
+          type="text"
+          name="text"
+          onChange={handleFormChange}
+        />
+
+        <TextField
+          value={form.title}
+          id="outlined-basic"
+          variant="outlined"
+          placeholder="Título"
+          type="text"
+          name="title"
+          onChange={handleFormChange}
+        />
+
+        <Button type="submit" variant="contained">
+          Postar
+        </Button>
+      </FormContainer>
+
       {posts.map((post) => (
         <PostCard
           key={post.id}
